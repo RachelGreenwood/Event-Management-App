afb4641b8662e5e8153fde4875c90548c639c4e7
Rachel Greenwood <Rachel.Greenwood@sony.com>
2025-10-23 14:54:33 -0500

    Adds Create Event Navigation Functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import ChooseRole from "./components/ChooseRole.jsx";
 import HomePage from "./components/HomePage.jsx";
 import Dashboard from "./components/Dashboard.jsx";
 import Navbar from "./components/Navbar.jsx";
+import CreateEvent from "./components/CreateEvent.jsx";
 
 function App() {
   const { loginWithRedirect, logout, user, isAuthenticated, getAccessTokenSilently } = useAuth0();
@@ -15,7 +16,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
       <Route path="/choose-role" element={<ChooseRole />} />
-      <Route path="/dashboard" element={<Dashboard />} />    
+      <Route path="/dashboard" element={<Dashboard />} />   
+      <Route path="/create-event" element={<CreateEvent />} /> 
     </Routes>
     </div>
   );
