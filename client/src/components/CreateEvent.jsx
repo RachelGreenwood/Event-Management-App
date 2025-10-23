ba6876a0713b0aee4918b68351f919eacfa2f4ec
Rachel Greenwood <Rachel.Greenwood@sony.com>
2025-10-23 15:14:52 -0500

    Adds Name To Event Creation Form

--- client/src/components/CreateEvent.jsx
+++ client/src/components/CreateEvent.jsx
@@ -1,3 +1,59 @@
+import React, { useState } from "react";
+
 export default function CreateEvent() {
-  return <h1>Create an Event</h1>;
+    const [formData, setFormData] = useState({
+        name: "",
+        event_date: "",
+        description: "",
+        ticket_types: [""],
+        prices: [""],
+        venue: "",
+        schedule: "",
+        performer: "",
+    });
+
+    // Updates form with user's input
+    const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+//   Handles submit button
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Sends event data to BE
+     try {
+      const res = await fetch("http://localhost:5000/events", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          ...formData,
+          prices: formData.prices.map(Number),
+          created_by: userId,
+        }),
+      });
+
+      const data = await res.json();
+      if (res.ok) {
+        alert("Event created successfully!");
+        console.log("Event:", data);
+      } else {
+        alert("Error creating event.");
+      }
+    } catch (err) {
+      console.error(err);
+      alert("Server error.");
+    }
+  };
+    
+  return (
+    <div>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Event Name: </label>
+                <input type="text" name="name" value={formData.name} onChange={handleChange} />
+            </div>
+        </form>
+    </div>
+  )
 }
