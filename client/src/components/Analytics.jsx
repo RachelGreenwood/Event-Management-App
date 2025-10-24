a026ed50209f2e8162ac0136292faf1e2c3706fc
Rachel Greenwood <Rachel.Greenwood@sony.com>
2025-10-24 16:09:47 -0500

    Adds Analytics Data Placeholder

--- client/src/components/Analytics.jsx
+++ client/src/components/Analytics.jsx
@@ -2,6 +2,12 @@ export default function Analytics() {
     return (
         <div>
             <h1>See Your Analytics</h1>
+            <p>Ticket Sales: </p>
+            <p>0</p>
+            <p>Attendance: </p>
+            <p>0</p>
+            <p>Revenue Earned (in USD): </p>
+            <p>$</p>
         </div>
     )
 }
